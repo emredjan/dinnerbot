bef6127c371a38541b93046e15e30e3eb26d042d
Emre Can <emredjan@gmail.com>
2017-05-18 21:59:45 +0200

    initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+# dinnerbot for Slack
+
+Testing the python client for slack to create a simple bot.
+
+Bsed on (Currently copy/paste :grin:) the excellent tutorial at [Full Stack Python](https://www.fullstackpython.com/blog/build-first-slack-bot-python.html)
---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# python-dotenv configuration
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# read secrets from environment variables
+SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+BOT_ID = os.environ.get('BOT_ID')
+
+# other constants
+BOT_NAME = 'dinnerbot'
